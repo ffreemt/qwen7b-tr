003436c262278f7d2ab22e30a0bbf3f1446977f7
ffreemt <yucongo+fmt@gmail.com>
2023-08-24 09:01:54 +0800

    Update direct nuitka build --follow-import-to=qwen7b_tr

--- qwen7b-tr.py
+++ qwen7b-tr.py
@@ -1,10 +1,10 @@
 """Prep for nuitka build."""
 # ruff: noqa: F401
-import gradio_client  # nuitka
-import loguru
-import pyperclip
-import rich
-import typer
+# import gradio_client  # nuitka
+# import loguru
+# import pyperclip
+# import rich
+# import typer
 
 from qwen7b_tr.__main__ import app
 
